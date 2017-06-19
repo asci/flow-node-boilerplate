9834107dc0773a62ac55dc553e55c77f1017cf39
Artem Riasnianskyi <asci@yandex.ru>
2017-06-19 22:34:46 +0200

    better scripts and example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-// @flow
-import type { Test } from './index.js.flow';
+import type { Test, Point2d } from './index.js.flow';
 
+const point: Point2d = {x: 2, y: 3};
 const test: Test = {
   adult: true,
   data: 'test',
   name: 'test',
-  age: 11
+  age: 11,
+  location: point
 };
 
-console.log('=========================start==========================');
-console.log(test.data, test.data);
+console.log('done');
